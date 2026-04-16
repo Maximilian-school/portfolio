b100bc0a2f1ba6ef229c6c7da272b87dde8f4d77
maximilian1121 <latificlynch@gmail.com>
2026-04-16 15:36:51 -0600

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,65 @@
 import Image from "next/image";
-import { supabase } from "../lib/supabaseClient";
-import Link from "next/link";
-import SocialLinks from "./socalLinks";
-import Languages from "./languages";
-import Technologies from "./tech";
 
-export default async function Page() {
-    const { data: top_langs, count } = await supabase
-        .from("top_langs")
-        .select("*", { count: "exact" })
-        .order("lines", { ascending: false });
-
-    return (
-        <div className="max-w-5xl py-12 space-y-8 mx-2 md:mx-auto">
-            <div
-                className="flex items-center justify-center gap-6"
-                id="profile-card"
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-                <Image
-                    alt="Profile picture"
-                    width={256}
-                    height={256}
-                    className="rounded-full h-32 w-32 md:h-64 md:w-64"
-                    src="/icon"
-                />
-                <div className="flex flex-col gap-7">
-                    <h1 className="text-2xl md:text-4xl font-semibold">
-                        Maximilian
-                    </h1>
-                    <h2 className="text-1xl md:text-2xl font-semibold">
-                        Average hobby developer
-                    </h2>
-                </div>
-            </div>
-            <div className="flex gap-10 align-top justify-center flex-col transition-all">
-                <div className="flex flex-col md:text-2xl text-lg justify-center align-middle">
-                    <h1 className="text-center text-4xl">About me</h1>
-                    <p className="mt-4 text-lg text-center">
-                        Hi, I'm Maximilian. I'm spending a lot of my time to
-                        make a single Roblox game. I cant say just yet what it
-                        it is or what it's about, but I can say that it has
-                        car's I love making games and I find that im really good
-                        at making funny additions to such games or Minecraft
-                        mods. Speaking of Minecraft mods, anyone who uses
-                        anything but fabric is WRONG! Fabric is my personal go
-                        to for making Mods It's API I remember by heart and I
-                        love using it. I also spend time working on side
-                        projects like this site. Thats not all tho, I also work
-                        on a few open source projects on GitHub. I also game,
-                        duh. My favorite games of all time in order are probably
-                        Boneworks, Minecraft, and Roblox (the game studio).
-                    </p>
-                </div>
-                <div className="flex flex-col gap-10 lg:gap-0 lg:flex-row md:text-2xl text-lg items-top w-full">
-                    <div className="flex-1 basis-1/2">
-                        <h1 className="text-center text-4xl w-full">
-                            Languages
-                        </h1>
-                        <Languages />
-                    </div>
-                    <div className="flex-1 basis-1/2">
-                        <h1 className="text-center text-4xl w-full">
-                            Technologies
-                        </h1>
-                        <Technologies />
-                    </div>
-                </div>
-                <div className="flex flex-col md:text-2xl text-lg justify-center align-middle">
-                    <h1 className="text-center text-4xl">Links</h1>
-                    <SocialLinks />
-                </div>
-            </div>
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
         </div>
-    );
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            new URL("https://avatars.githubusercontent.com/u/168935358"),
-        ],
-    },
+  /* config options here */
 };
 
 export default nextConfig;
